a1f0fdf1cc4ee522796bdc5c96865ea93376b38e
Uchchash346 <mru416@gmail.com>
2022-02-26 21:01:08 +0600

    Completed work

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,63 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 import './App.css';
+import TodoTask from './components/TodoTask';
+import { ITask } from './Interfaces'
 
 const App: FC = () => {
+
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline }
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDeadline(0);
+  }
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(todoList.filter((task) => {
+      return task.taskName !== taskNameToDelete
+    }))
+  }
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task..." id="" />
-          <input type="number" placeholder="Deadline (in Days)..." id="" />
+          <input
+            type="text"
+            name="task"
+            placeholder="Task..."
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="deadline"
+            placeholder="Deadline (in Days)..."
+            value={deadline}
+            onChange={handleChange}
+          />
         </div>
-        <button>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       </div>
       <div className="todoList">
-
+        {
+          todoList.map((task: ITask, key: number) => {
+            console.log(todoList)
+            return <TodoTask key={key} task={task} completeTask={completeTask} />;
+          })
+        }
       </div>
     </div>
   );
